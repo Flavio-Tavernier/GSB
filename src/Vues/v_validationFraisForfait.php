c15378be3ce822461ee7b31b43efc9d505e00924
Flavio TAVERNIER <Flavio.TAVERNIER@bts.sio>
2024-08-30 11:59:05 +0200

    faire methode getVisiteurs

---
+++ src/Vues/v_validationFraisForfait.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * Vue Liste des frais au forfait
+ *
+ * PHP Version 8
+ *
+ * @category  PPE
+ * @package   GSB
+ * @author    Réseau CERTA <contact@reseaucerta.org>
+ * @author    José GIL <jgil@ac-nice.fr>
+ * @copyright 2017 Réseau CERTA
+ * @license   Réseau CERTA
+ * @version   GIT: <0>
+ * @link      http://www.reseaucerta.org Contexte « Laboratoire GSB »
+ * @link      https://getbootstrap.com/docs/3.3/ Documentation Bootstrap v3
+ */
+
+?>
+<div class="row">     
+    <label for="lstVisiteur">Choisir le visiteur :</label> 
+    <select name="lstVisiteur" id="lstVisiteur">
+    <!-- Faire boucle pour générer options -->
+    </select> 
+    
+    <label for="lstDateFicheFrais">Mois :</label>
+    <select name="lstDateFicheFrais" id="lstDateFicheFrais">
+    <!-- Faire boucle pour générer options -->
+    </select> 
+    
+    
+    <!-- <h2>
+        Valider la fiche de frais 
+    </h2>
+    <h3>Eléments forfaitisés</h3>
+    <div class="col-md-4">
+        <form method="post" 
+              action="index.php?uc=gererFrais&action=validerMajFraisForfait" 
+              role="form">
+            <fieldset>       
+                <?php
+                foreach ($lesFraisForfait as $unFrais) {
+                    $idFrais = $unFrais['idfrais'];
+                    $libelle = htmlspecialchars($unFrais['libelle']);
+                    $quantite = $unFrais['quantite']; ?>
+                    <div class="form-group">
+                        <label for="idFrais"><?php echo $libelle ?></label>
+                        <input type="text" id="idFrais" 
+                               name="lesFrais[<?php echo $idFrais ?>]"
+                               size="10" maxlength="5" 
+                               value="<?php echo $quantite ?>" 
+                               class="form-control">
+                    </div>
+                    <?php
+                }
+                ?>
+                <button class="btn btn-success" type="submit">Ajouter</button>
+                <button class="btn btn-danger" type="reset">Effacer</button>
+            </fieldset>
+        </form>
+    </div> -->
+</div>
